fea2e3ce031efe189e574d8c43d8b31b23bd626b
destinyinsurancegroup-a11y <destinyinsurancegroup@gmail.com>
2025-10-29 10:27:36 -0400

    Create settings.php

---
+++ resources/views/settings.php
@@ -0,0 +1,20 @@
+<div class="card form-card" style="max-width:880px;">
+  <h3>Profile Settings</h3>
+  <div class="row">
+    <div class="input"><label>First Name</label><input placeholder="Richard"></div>
+    <div class="input"><label>Last Name</label><input placeholder="Dehn"></div>
+  </div>
+  <div class="row">
+    <div class="input"><label>Email</label><input placeholder="you@agency.com"></div>
+    <div class="input"><label>Phone</label><input placeholder="(555) 555-5555"></div>
+  </div>
+  <div class="row">
+    <div class="input"><label>Agency Name</label><input placeholder="Destiny Insurance Group"></div>
+    <div class="input"><label>Timezone</label>
+      <select>
+        <option>EST</option><option>CST</option><option>MST</option><option>PST</option>
+      </select>
+    </div>
+  </div>
+  <button class="btn">Save Settings</button>
+</div>
