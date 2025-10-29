b4dac2d90a5304b8fbeaeca2db042f4b7e423b03
destinyinsurancegroup-a11y <destinyinsurancegroup@gmail.com>
2025-10-29 10:25:32 -0400

    Create service.php

---
+++ resources/views/service.php
@@ -0,0 +1,13 @@
+<div class="card" style="width:100%;">
+  <h3>Service Queue (Clients Needing Attention)</h3>
+  <table>
+    <thead>
+      <tr><th>Client</th><th>Issue</th><th>Priority</th><th>Due</th><th>Status</th></tr>
+    </thead>
+    <tbody>
+      <tr><td>Mary White</td><td>Payment lapse</td><td>High</td><td>2025-10-30</td><td>Open</td></tr>
+      <tr><td>Paul Brown</td><td>Address change</td><td>Normal</td><td>2025-11-01</td><td>Open</td></tr>
+      <tr><td>Susan Ray</td><td>Beneficiary update</td><td>Normal</td><td>2025-11-04</td><td>Scheduled</td></tr>
+    </tbody>
+  </table>
+</div>
