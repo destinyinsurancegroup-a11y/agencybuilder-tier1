c52067499d30b3e9f9b663f37b9f1c0c4a0da3b2
destinyinsurancegroup-a11y <destinyinsurancegroup@gmail.com>
2025-10-29 10:24:06 -0400

    Create all_contacts.php

---
+++ resources/views/all_contacts.php
@@ -0,0 +1,11 @@
+<div class="card form-card" style="width:100%;">
+  <h3>All Contacts (Demo Data)</h3>
+  <table>
+    <thead><tr><th>Name</th><th>Status</th><th>Source</th><th>Phone</th><th>Email</th></tr></thead>
+    <tbody>
+      <tr><td>John Scott</td><td>Prospect</td><td>Web</td><td>(555) 201-1122</td><td>john@example.com</td></tr>
+      <tr><td>Sally Jenkins</td><td>Client</td><td>Referral</td><td>(555) 312-4455</td><td>sally@example.com</td></tr>
+      <tr><td>Richard R. Dehn</td><td>Prospect</td><td>Facebook</td><td>(555) 777-8822</td><td>richard@example.com</td></tr>
+    </tbody>
+  </table>
+</div>
